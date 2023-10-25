6db8987d91eac4d39687dd826f81abcc51d2965a
jijimenezf <jijimenezf@hotmail.com>
2023-10-25 14:07:01 -0600

    Trivial change to run workflow

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,5 +2,6 @@ import app from 'lib/createServer'
 
 app.listen(3000, () => {
   console.log('...setting up local environment')
+  console.log('and waiting for everyting is working as expected')
   console.log(`🚀 Server ready at: http://localhost:3000`)
 })
